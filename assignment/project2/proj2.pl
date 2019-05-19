6b256997d8af0e99c1f2832a998ee7b4f0127c17
Hsnyql <yuqielu@live.com>
2019-05-19 19:36:12 +1000

    add proj2

--- assignment/project2/proj2.pl
+++ assignment/project2/proj2.pl
@@ -1,64 +1,80 @@
-%There are four constraints for the math puzzles:
-%1.All the digonals should be same value.
-%2.The heading of row holds either the sum or the product of all the digits in that row.
-%3.The heading of column holds either the sum or the product of all the digits in that column.
-%4.Each row and column contains no repeated digits (from 1 to 9).
+% Author: Qielu Yu, qieluy@student.unimelb.edu.au
+% Student number: 962682
+
+% The objective of this project is to practice and 
+% assess understanding of logic programming and Prolog.
+
+% A maths puzzle is a square grid of squares, each to be filled in with 
+% a single digit 1–9 (zero is not permitted) satisfying these constraints:
+% • each row and each column contains no repeated digits;
+% • all squares on the diagonal line from upper left to lower right contain 
+%   the same value;
+% • the heading of reach row and column (leftmost square in a row and topmost 
+%   square in a column) holds either the sum or the product of all the digits 
+%   in that row or column
+
 
 :- ensure_loaded(library(clpfd)).
 
-puzzle_solution([Headings|Tails]):- %follow these 4 constraints above.
-	constraint_digonal(Tails,1,1,_),
-	constraint_column([Headings|Tails]),
-	constraint_row(Tails),
-	%write(Puzzle).
-	maplist(labeling([]),[Headings|Tails]). %Show the puzzle.
+% digonal/4
+% Get the digunal vuale, the number of row is the index on each row.
+digonal([H|_], 0, H).
+digonal([_|T], Row, Dig):-
+    Row1 is Row - 1,
+    digonal(T, Row1, Dig).
 
-constraint_digonal([],_,_,_).  
-constraint_digonal([Hs|Ts],Row,Index,Digonal):- %Check whether the digonal is valid solution(except the first row).
-							   %The first argument is the remaining rows except the first row.
-							   %The second means which row the function is operating on. 
-							   %The third is the index of the digonal.
-							   %The fourth is the value of digonal.
-	digonal_value(Hs,Row,Index,Digonal), %Take the first row of rows.
-	Row1 is Row + 1,
-	Index1 is Row1,
-	constraint_digonal(Ts,Row1,Index1,Digonal).
 
-digonal_value([Head|_],_,0,Head). %Find the value of digonal.
-digonal_value([_|Tail],_,Index,Value):-
-	Index0 is Index - 1, %Have to subtract 1 when first time since it is the heading. 
-	digonal_value(Tail,_,Index0,Value).
-	  
-constraint_column([]).
-constraint_column([Hs|Ts]):- %Transpose columns to rows.
-	transpose([Hs|Ts],[_|Rowts]), %Then operate on the remaining rows except the first row.
-	constraint_row(Rowts).
+% digonal/4
+% The digonal values should be same
+digonal_constraint([], _, _).
+digonal_constraint([Hs|Ts], Row, Dig):-
+    digonal(Hs, Row, Dig),
+    Row1 is Row + 1,
+    digonal_constraint(Ts, Row1, Dig).
 
-constraint_row([]).
-constraint_row([Hs|Ts]):- %Operate on the a row of the remaining rows.
-	single_row(Hs),
-	%maplist(all_distinct,Hs), 
-	all_distinct(Hs), %Check whether solutions in the Hs are distinct.
-	constraint_row(Ts).
+% row_sum/2
+% calculate the sum of a row
+row_sum([], 0).
+row_sum([H|T], X):-
+    between(1, 9, H),
+    row_sum(T, X1),
+    X is H + X1.
 
-single_row([Value|Tail]):- %Check whether the sum or product of elements in the tail equals to the value.
-	sum_row(Value,Tail);
-	product_row(Value,Tail).
+% row_pro/2
+% calculate the product of a row
+row_product([],1).
+row_product([H|T], X):-
+    between(1, 9, H),
+    row_product(T, X1),
+    X is X1 * H.
 
-sum_row(0,[]).
-sum_row(Value,[Head|Tail]):- %Sum the elements in the row and leave the result in the value.
-	sum_row(Value1,Tail),
-	%maplist(Head,Vs), %Vs ins 1..9,
-	Head #>= 1, Head #=< 9,
-	%Value is Value1 + Head.
-	Value #= Value1 + Head.
+% row/1
+% check if the heading value equals to the sum or product in a row.
+% the structure of the matrix shows that the heading row and column are the 
+% the sum or product.
+row([Value|Row]):-
+    row_sum(Row, Value);
+    row_product(Row, Value).
 
-product_row(1,[]).
-product_row(Value,[Head|Tail]):- %Product the elements in the row and leave the result in the value.
-	product_row(Value1,Tail),
-	Head #>= 1, Head #=< 9,
-	%Value is Value1 * Head.
-	Value #= Value1 * Head.
+% row_constraint/1
+% apply the row/1 to each row
+% the elements of a row should be distinct
+row_constraint([]).
+row_constraint([H|T]):-
+    row(H),
+    all_distinct(H),
+    row_constraint(T).
+
+% puzzle_solution/1
+% the requied answer of the project.
+% apply the constraint method of digonal, rows and columns
+% transpose the rows to columns, the first column is the sum or product.
+puzzle_solution([H|T]):-
+    digonal_constraint(T, 1, _),
+    transpose([H|T], [_|Col]),
+    row_constraint(T),
+    row_constraint(Col),
+    maplist(labeling([]),[H|T]).
 
 
 
